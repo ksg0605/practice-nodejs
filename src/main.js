29b200e9a4c0ef54439b708389951205e2ef720a
ksg0605 <ksg0605@gmail.com>
2021-10-31 19:42:18 +0900

    단순한 API라우팅 처리와 HTTPie를 이용한 테스팅

--- src/main.js
+++ src/main.js
@@ -8,3 +8,38 @@
  * - 인증 로직은 넣지 않습니다.
  * - RESTful API를 사용합니다.
  */
+
+const http = require('http');
+
+/**
+ * Post
+ *
+ * GET /posts
+ * GET /posts/:id
+ * POST /posts
+ */
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/posts' && req.method === 'GET') {
+    res.statusCode = 200;
+    res.end('List of the posts');
+  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+    res.statusCode = 200;
+    res.end('Some content of the post');
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200;
+    res.end('Creating post');
+  } else {
+    res.statusCode = 404;
+    res.end('Not found.');
+  }
+
+  res.statusCode = 200;
+  res.end('Hello!');
+});
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`The server is listening at port: ${PORT}`);
+});
