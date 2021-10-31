f966c2368b6a754dd8f8bb0c429d9b03d0cc59b1
ksg0605 <ksg0605@gmail.com>
2021-10-31 20:42:50 +0900

    모든 API 완성하기

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@
  */
 
 const http = require('http');
+const { encode } = require('punycode');
 
 /**
  * @typedef Post
@@ -27,7 +28,7 @@ const posts = [
   },
   {
     id: 'my_second_post',
-    title: 'My second post',
+    title: '나의 두번째 포스트',
     content: 'Second post!',
   },
 ];
@@ -46,15 +47,54 @@ const server = http.createServer((req, res) => {
     (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined;
 
   if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      post: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    };
+
     res.statusCode = 200;
-    res.end('List of the posts');
-  } else if (postIdRegexResult) {
+    res.setHeader('Content-Type', 'application/json; encoding=utf-8');
+    res.end(JSON.stringify(result));
+  } else if (postIdRegexResult && req.method === 'GET') {
     // GET /posts/:id
     const postId = postIdRegexResult[1];
-    console.log(`postId: ${postId}`);
+
+    const post = posts.find((_post) => _post.id === postId);
+
+    if (post) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json; encoding=utf-8');
+      res.end(JSON.stringify(post));
+    } else {
+      res.statusCode = 404;
+      res.end('Post not found');
+    }
+
     res.statusCode = 200;
     res.end('Reading a post');
   } else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8');
+    req.on('data', (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+
+      /**
+       * @type {CreatePostBody}
+       */
+      const body = JSON.parse(data);
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, '_'),
+        title: body.title,
+        content: body.content,
+      });
+    });
+
     res.statusCode = 200;
     res.end('Creating post');
   } else {
